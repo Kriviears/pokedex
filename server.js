e5bd5b7c5677dc063e0eefb52afe2ee456e2a80b
kriviears <kriviears@gmail.com>
2019-07-10 14:35:28 -0400

    Ready for deploy on heroku

--- server.js
+++ server.js
@@ -1,20 +1,78 @@
 'use strict';
 
-console.log('hello');
 
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
+const cors = require('cors');
+const helmet = require('helmet');
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res)=>{
-  res.send('Hello, world');
+
+app.use(function validateBearerToken(req, res, next){
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({ error: 'Unauthorized request'});
+  }
+  next();
 });
 
-const PORT = 8000;
+app.use((error, req, res, next)=>{
+  let response;
+  if (process.env.NODE_ENV === 'production'){
+    response = { error: { message: 'server error'}};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
+
+const validTypes = [
+  'Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting',
+  'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice',
+  'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'
+];
+
+function handleGetTypes(req, res){
+  res.json(validTypes);
+}
+
+function handleGetPokemon(req, res){
+  let response = POKEDEX.pokemon;
+  const { name, type } = req.query;
+
+  if (name){
+    response = response.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(name.toLowerCase())
+    );
+  }
+
+  if(type){
+    response = response.filter(pokemon =>
+      pokemon.type.includes(type)
+    );
+  }
+
+  res.json(response);
+}
+
+app.get('/types', handleGetTypes);
+
+app.get('/pokemon', handleGetPokemon);
+
+
 
 app.listen(PORT, ()=>{
-  console.log(`Server listening at http://localhost:${PORT}`);
+
 });
